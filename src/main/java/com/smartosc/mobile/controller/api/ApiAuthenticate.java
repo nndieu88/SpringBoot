5984e1cd3f284839bdc416a67ac127be5991befd
nnd <nndieu88@gmail.com>
2020-03-21 13:10:11 +0700

    test-security

--- src/main/java/com/smartosc/mobile/controller/api/ApiAuthenticate.java
+++ src/main/java/com/smartosc/mobile/controller/api/ApiAuthenticate.java
@@ -13,10 +13,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+@RestController
 public class ApiAuthenticate {
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
